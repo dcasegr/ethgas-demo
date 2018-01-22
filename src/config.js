fc60a81c433a722b419658d5b4b8181c79dbbb21
David Case <david.case@envoyplatform.com>
2018-01-22 00:59:57 -0500

    update config

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ const config = {
     rpc: "http://localhost:8545",
 
     // Use MetaMask browser node. NOTE: MetaMask does not allow access to pending transactions, so data will be incomplete.
-    enableMetaMask: true,
+    enableMetaMask: false,
 };
 
 export default config;
